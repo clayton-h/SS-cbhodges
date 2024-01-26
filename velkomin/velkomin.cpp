2d1bb38a86a26da04f5c9f737ef0ffcce8ccd774
ClayT <claytonhodges326@gmail.com>
2024-01-26 09:54:55 -0700

    velkomin

---
+++ velkomin/velkomin.cpp
@@ -0,0 +1,13 @@
+/*
+    C++ script that prints "VELKOMIN!"
+
+    By: Clayton H.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "VELKOMIN!";
+    return 0;
+}
---
+++ velkomin/velkomin.py
@@ -0,0 +1,12 @@
+#
+# Python script that prints "VELKOMIN!"
+#
+# By: Clayton H.
+#
+
+def main() -> None:
+    print("VELKOMIN!")
+
+
+if __name__ == "__main__":
+    main()
